f9a96780de8cddb5cb389595388429c76af298ec
赵强 <568423952@qq.com>
2021-07-21 13:45:04 +0800

    创建了新的文件

---
+++ Git-GitHubTest3/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Git-GitHubTest3
+//
+//  Created by ZhaoQ on 2021/7/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //你好
+        print("你好吗")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
