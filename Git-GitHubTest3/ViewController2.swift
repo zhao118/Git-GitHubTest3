f5e7b04e2dd07ed452d51012db028fe938314195
赵强 <568423952@qq.com>
2021-10-18 09:16:59 +0800

    修改三

--- Git-GitHubTest3/ViewController2.swift
+++ Git-GitHubTest3/ViewController2.swift
@@ -17,14 +17,7 @@ class ViewController2: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func xx() {
+        print("7777777")
     }
-    */
-
 }
